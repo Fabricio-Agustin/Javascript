ac7f3676783c5af6a8de8ba649dc0e8268a30a9e
Fabricio-Agustin <agustingibautcarrizo@gmail.com>
2026-02-16 17:24:04 -0300

    constantes

---
+++ constantes/constante.js
@@ -0,0 +1,22 @@
+/*
+constante de javascript.
+
+Una constante es una varable que una vez definido su
+valor. ya no se puede modificar.
+
+una constante como buenas practica se define con una
+letra mayuscula. y si el nombre de la constante es
+compuesto se separa con guion bajo.
+*/
+
+const mi_constante = 'Hola mundo'; // no podemos modificar el valor de esta constante, es decir, no podemos hacer mi_constante = 'Adios_mundo';
+
+// Imprimimos el valor de la constante en la consola.
+console.log(mi_constante);
+
+// definiremos otra constante segundos por minuto
+const SEGUNDOS_POR_MINUTO = 60; // esta constante representa el numero de segundos que hay en un minuto, y no se puede modificar su valor
+
+// Imprimimos el valor de la constante en la consola.
+console.log(SEGUNDOS_POR_MINUTO);
+
