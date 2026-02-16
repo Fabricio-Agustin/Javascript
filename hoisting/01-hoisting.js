1af1e6cc2edec036d1257187e7ab2190fda89fef
Fabricio-Agustin <agustingibautcarrizo@gmail.com>
2026-02-16 13:02:54 -0300

    hoisting

---
+++ hoisting/01-hoisting.js
@@ -0,0 +1,17 @@
+//hoisting javascript.
+
+/*
+Las declaraciones de variables
+puedes estar en cualquier parte del
+programa (hoisting).
+*/
+
+
+let x; //declaracion de variables
+x = 20; //inicializacion de variables
+console.log(x);//mostrat en consola.
+
+
+y = 10;
+var y;
+console.log(y); // 10
